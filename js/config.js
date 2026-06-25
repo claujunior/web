7ce98a7a1d0e9f549ead6f632f71a9a6f3c69dba
claujunior <claudivanjunior2002@hotmail.com>
2026-06-25 04:10:36 -0300

    false

--- js/config.js
+++ js/config.js
@@ -1,7 +1,8 @@
 export const API_URL = 'http://127.0.0.1:8000';
 
 export function nsfwEnabled() {
-    return localStorage.getItem("nsfw") !== "0";
+    return false; 
+    //localStorage.getItem("nsfw") !== "0";
 }
 
 export function setNsfwEnabled(on) {
